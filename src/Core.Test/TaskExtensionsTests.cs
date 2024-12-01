301183d9acd9edc8d3aec6f7986c3dfbd147f608
Charles Farris <charlesfarris71@gmail.com>
2024-12-01 01:31:13 -0700

    Add async version of Result methods

---
+++ src/Core.Test/TaskExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿namespace SleepingBear.Monad.Core.Test;
+
+/// <summary>
+///     Tests for <see cref="TaskExtensions" />.
+/// </summary>
+internal static class TaskExtensionsTests
+{
+    [Test]
+    public static async Task BindAsync_Invalid_ThrowsInvalidOperationException()
+    {
+        await Task.Delay(0).ConfigureAwait(false);
+        _ = Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            _ = await new Result<int>()
+                .ToTask()
+                .BindAsync(ok => ok.ToOk().ToTask())
+                .ConfigureAwait(false);
+        });
+    }
+
+    [Test]
+    public static async Task BindFailureAsync_Invalid_ThrowsInvalidOperationException()
+    {
+        await Task.Delay(0).ConfigureAwait(false);
+        _ = Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            _ = await new Result<int>()
+                .ToTask()
+                .BindFailureAsync(error => error.ToFailure<int>().ToTask())
+                .ConfigureAwait(false);
+        });
+    }
+
+    [Test]
+    public static async Task MapAsync_Invalid_ThrowsInvalidOperationException()
+    {
+        await Task.Delay(0).ConfigureAwait(false);
+        _ = Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            _ = await new Result<int>()
+                .ToTask()
+                .MapAsync(ok => ok.ToTask())
+                .ConfigureAwait(false);
+        });
+    }
+
+    [Test]
+    public static async Task MapFailureAsync_Invalid_ThrowsInvalidOperationException()
+    {
+        await Task.Delay(0).ConfigureAwait(false);
+        _ = Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            _ = await new Result<int>()
+                .ToTask()
+                .MapFailureAsync(error => error.ToTask())
+                .ConfigureAwait(false);
+        });
+    }
+
+    [Test]
+    public static async Task MatchAsync_Invalid_ThrowsInvalidOperationException()
+    {
+        await Task.Delay(0).ConfigureAwait(false);
+        _ = Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            _ = await new Result<int>()
+                .ToTask()
+                .MatchAsync(_ => 0.ToTask(), _ => 1.ToTask())
+                .ConfigureAwait(false);
+        });
+    }
+}
---
+++ src/Core/TaskExtensions.cs
@@ -0,0 +1,167 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SleepingBear.Monad.Core;
+
+/// <summary>
+///     Extension methods for <see cref="Task{TResult}" />.
+/// </summary>
+public static class TaskExtensions
+{
+    /// <summary>
+    ///     Extension method to convert a value to a <see cref="Task{TResult}" />.
+    /// </summary>
+    /// <param name="result">The task value.</param>
+    /// <typeparam name="TResult">The value type.</typeparam>
+    /// <returns>A <see cref="Task{TResult}" />.</returns>
+    public static Task<TResult> ToTask<TResult>(this TResult result)
+    {
+        return Task.FromResult(result);
+    }
+
+    /// <summary>
+    ///     Async version of the bind method of <see cref="Result{TOk}" />.
+    /// </summary>
+    /// <param name="task">The task.</param>
+    /// <param name="bind">The binding function.</param>
+    /// <typeparam name="TOk">The OK type.</typeparam>
+    /// <typeparam name="TOkOut">The output OK type.</typeparam>
+    /// <returns>A <see cref="Task{TResult}" />.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if state is Invalid.</exception>
+    /// <exception cref="UnreachableException">Thrown if the state is unknown.</exception>
+    [SuppressMessage("ReSharper", "NullableWarningSuppressionIsUsed")]
+    public static async Task<Result<TOkOut>> BindAsync<TOk, TOkOut>(
+        this Task<Result<TOk>> task,
+        Func<TOk, Task<Result<TOkOut>>> bind)
+    {
+        ArgumentNullException.ThrowIfNull(task);
+        ArgumentNullException.ThrowIfNull(bind);
+
+        var result = await task.ConfigureAwait(false);
+        var (state, ok, error) = result;
+        return state switch
+        {
+            ResultState.Ok => await bind(ok!).ConfigureAwait(false),
+            ResultState.Failure => new Result<TOkOut>(error!),
+            ResultState.Invalid => throw new InvalidOperationException(),
+            _ => throw new UnreachableException()
+        };
+    }
+
+    /// <summary>
+    ///     Async version of the bind failure method of <see cref="Result{TOk}" />.
+    /// </summary>
+    /// <param name="task">The task.</param>
+    /// <param name="bind">The binding function.</param>
+    /// <typeparam name="TOk">The OK type.</typeparam>
+    /// <returns>A <see cref="Task{TResult}" />.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if state is Invalid.</exception>
+    /// <exception cref="UnreachableException">Thrown if the state is unknown.</exception>
+    [SuppressMessage("ReSharper", "NullableWarningSuppressionIsUsed")]
+    public static async Task<Result<TOk>> BindFailureAsync<TOk>(
+        this Task<Result<TOk>> task,
+        Func<Error, Task<Result<TOk>>> bind)
+    {
+        ArgumentNullException.ThrowIfNull(task);
+        ArgumentNullException.ThrowIfNull(bind);
+
+        var result = await task.ConfigureAwait(false);
+        var (state, _, error) = result;
+        return state switch
+        {
+            ResultState.Ok => result,
+            ResultState.Failure => await bind(error!).ConfigureAwait(false),
+            ResultState.Invalid => throw new InvalidOperationException(),
+            _ => throw new UnreachableException()
+        };
+    }
+
+    /// <summary>
+    ///     Async version of the map method of <see cref="Result{TOk}" />.
+    /// </summary>
+    /// <param name="task">The task.</param>
+    /// <param name="bind">The mapping function.</param>
+    /// <typeparam name="TOk">The OK type.</typeparam>
+    /// <typeparam name="TOkOut">The output OK type.</typeparam>
+    /// <returns>A <see cref="Task{TResult}" />.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if state is Invalid.</exception>
+    /// <exception cref="UnreachableException">Thrown if the state is unknown.</exception>
+    [SuppressMessage("ReSharper", "NullableWarningSuppressionIsUsed")]
+    public static async Task<Result<TOkOut>> MapAsync<TOk, TOkOut>(
+        this Task<Result<TOk>> task,
+        Func<TOk, Task<TOkOut>> bind)
+    {
+        ArgumentNullException.ThrowIfNull(task);
+        ArgumentNullException.ThrowIfNull(bind);
+
+        var result = await task.ConfigureAwait(false);
+        var (state, ok, error) = result;
+        return state switch
+        {
+            ResultState.Ok => new Result<TOkOut>(await bind(ok!).ConfigureAwait(false)),
+            ResultState.Failure => new Result<TOkOut>(error!),
+            ResultState.Invalid => throw new InvalidOperationException(),
+            _ => throw new UnreachableException()
+        };
+    }
+
+    /// <summary>
+    ///     Async version of the map failure method of <see cref="Result{TOk}" />.
+    /// </summary>
+    /// <param name="task">The task.</param>
+    /// <param name="bind">The mapping function.</param>
+    /// <typeparam name="TOk">The OK type.</typeparam>
+    /// <returns>A <see cref="Task{TResult}" />.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if state is Invalid.</exception>
+    /// <exception cref="UnreachableException">Thrown if the state is unknown.</exception>
+    [SuppressMessage("ReSharper", "NullableWarningSuppressionIsUsed")]
+    public static async Task<Result<TOk>> MapFailureAsync<TOk>(
+        this Task<Result<TOk>> task,
+        Func<Error, Task<Error>> bind)
+    {
+        ArgumentNullException.ThrowIfNull(task);
+        ArgumentNullException.ThrowIfNull(bind);
+
+        var result = await task.ConfigureAwait(false);
+        var (state, _, error) = result;
+        return state switch
+        {
+            ResultState.Ok => result,
+            ResultState.Failure => new Result<TOk>(await bind(error!).ConfigureAwait(false)),
+            ResultState.Invalid => throw new InvalidOperationException(),
+            _ => throw new UnreachableException()
+        };
+    }
+
+    /// <summary>
+    ///     Async version of the match method of <see cref="Result{TOk}" />.
+    /// </summary>
+    /// <param name="task">The task.</param>
+    /// <param name="okFunc">The OK function.</param>
+    /// <param name="errorFunc">The error function.</param>
+    /// <typeparam name="TOk">The OK type.</typeparam>
+    /// <typeparam name="TOut">The output type.</typeparam>
+    /// <returns>A <see cref="Task{TResult}" />.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if state is Invalid.</exception>
+    /// <exception cref="UnreachableException">Thrown if the state is unknown.</exception>
+    [SuppressMessage("ReSharper", "NullableWarningSuppressionIsUsed")]
+    public static async Task<TOut> MatchAsync<TOk, TOut>(
+        this Task<Result<TOk>> task,
+        Func<TOk, Task<TOut>> okFunc,
+        Func<Error, Task<TOut>> errorFunc)
+    {
+        ArgumentNullException.ThrowIfNull(task);
+        ArgumentNullException.ThrowIfNull(okFunc);
+        ArgumentNullException.ThrowIfNull(errorFunc);
+
+        var result = await task.ConfigureAwait(false);
+        var (state, ok, error) = result;
+        return state switch
+        {
+            ResultState.Ok => await okFunc(ok!).ConfigureAwait(false),
+            ResultState.Failure => await errorFunc(error!).ConfigureAwait(false),
+            ResultState.Invalid => throw new InvalidOperationException(),
+            _ => throw new UnreachableException()
+        };
+    }
+}
