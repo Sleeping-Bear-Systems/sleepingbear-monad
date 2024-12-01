c85ff316386e9dd255e4c7b2aaf8246d4f2bff37
Charles Farris <charlesfarris71@gmail.com>
2024-11-30 23:31:08 -0700

    Add Error class
    - Add tests

---
+++ src/Core/Error.cs
@@ -0,0 +1,20 @@
+﻿namespace SleepingBear.Monad.Core;
+
+/// <summary>
+/// Abstract error base class.
+/// </summary>
+public abstract class Error;
+
+/// <summary>
+/// Concrete error that wraps a single value.
+/// </summary>
+/// <typeparam name="TValue">The value type.</typeparam>
+public sealed class Error<TValue> : Error
+{
+    internal Error(TValue value) => this.Value = value;
+
+    /// <summary>
+    /// Gets the error value.
+    /// </summary>
+    public TValue Value { get; }
+}
