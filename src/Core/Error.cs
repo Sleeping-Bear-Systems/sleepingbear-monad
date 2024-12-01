f9153e042f2a4dc004624dde3a0db13c12a63152
Charles Farris <charlesfarris71@gmail.com>
2024-12-01 16:00:55 -0700

    Convert Error to record class

--- src/Core/Error.cs
+++ src/Core/Error.cs
@@ -6,22 +6,11 @@ namespace SleepingBear.Monad.Core;
 ///     Abstract error base class.
 /// </summary>
 [SuppressMessage("Naming", "CA1716:Identifiers should not match keywords")]
-public abstract class Error;
+public abstract record Error;
 
 /// <summary>
 ///     Concrete error that wraps a single value.
 /// </summary>
 /// <typeparam name="TValue">The value type.</typeparam>
 [SuppressMessage("Naming", "CA1716:Identifiers should not match keywords")]
-public sealed class Error<TValue> : Error
-{
-    internal Error(TValue value)
-    {
-        this.Value = value;
-    }
-
-    /// <summary>
-    ///     Gets the error value.
-    /// </summary>
-    public TValue Value { get; }
-}
+public sealed record Error<TValue>(TValue Value) : Error where TValue : notnull;
